de09ff3a284b9f3c6b14e366b0fd2b2d795265f9
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-17 19:41:38 -0300

    iniciando projeto

---
+++ ng-cash-front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/reset.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project here</h1>
+    </div>
+  );
+}
+
+export default App;
