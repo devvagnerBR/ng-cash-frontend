05f758b30ab819d630bc1697883e57af0d617201
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-21 10:56:41 -0300

    wallet finalizada

--- ng-cash-front-end/src/pages/homepage/Homepage.tsx
+++ ng-cash-front-end/src/pages/homepage/Homepage.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import eyeBalance from '../../assets/icons/eyeBalance.png'
+import nglogo from '../../assets/nglogo.png'
+import logout from '../../assets/icons/logout.png'
 const Homepage = () => {
 
     return (
@@ -19,17 +21,30 @@ const Homepage = () => {
                             <img src={eyeBalance} alt="show balance" />
                         </div>
                     </main>
-
+                    <nav className='wallet-buttons'>
+                        <button className='btn' >transferir</button>
+                        <button className='btn' >atividade</button>
+                    </nav>
                 </section>
 
                 <section className='card-preview-homepage'>
 
+                    <main className='card-homepage'>
+                        <img src={nglogo} alt="" />
+
+                        <h1>WAGNER GUIMARAES</h1>
+                        <h1 id='number-card-homepage'>0000 0000 0000 0000</h1>
+                    </main>
+                    <p>modalidade: <span>DÉBITO</span> </p>
+
+                </section>
+                <section className='logout-homepage'>
+                    <div className='icon-logout'>
+                        <img src={logout} alt="" />
+                        <a href="">SAIR</a>
+                    </div>
                 </section>
             </aside>
-
-
-
-
 
             <main className='main-content-homepage'>
 
